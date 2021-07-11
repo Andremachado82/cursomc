c9f084199e2cd78ec44d15b42a230b9bd09d70ad
Andremachado82 <andrels.machado82@gmail.com>
2021-07-11 15:43:30 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/andremachado/cursomc/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.andremachado.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andremachado.cursomc.domain.Pedido;
+import com.andremachado.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService pedidoService;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+		
+		Pedido pedido = pedidoService.buscarPedidoPorId(id);
+			
+		return ResponseEntity.ok().body(pedido);
+	}
+	
+}
