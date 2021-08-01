23750659ae5bfd0a9596720b0bb4dcb31976b43e
Andremachado82 <andrels.machado82@gmail.com>
2021-08-01 12:51:19 -0300

    Inserido método insert da Categoria

--- src/main/java/com/andremachado/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andremachado/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,47 @@
 package com.andremachado.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andremachado.cursomc.domain.Categoria;
 import com.andremachado.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
-		
+
 		Categoria categoria = categoriaService.buscarCategoriaPorId(id);
-			
+
 		return ResponseEntity.ok().body(categoria);
 	}
-	
+
+	@PostMapping()
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = categoriaService.insert(categoria);
+
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 }
