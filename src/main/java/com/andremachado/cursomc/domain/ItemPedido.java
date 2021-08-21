1ff6d454807c2dc0b167cedbb0613e9f8e7aa5e2
Andremachado82 <andrels.machado82@gmail.com>
2021-08-21 19:09:59 -0300

    Subtotal e total do pedido

--- src/main/java/com/andremachado/cursomc/domain/ItemPedido.java
+++ src/main/java/com/andremachado/cursomc/domain/ItemPedido.java
@@ -22,6 +22,7 @@ public class ItemPedido implements Serializable {
 	
 	private Double preco;
 	
+	
 	public ItemPedido() {
 	}
 
@@ -32,6 +33,10 @@ public class ItemPedido implements Serializable {
 		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
+	}
+	
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
 	}
 	
 	@JsonIgnore
