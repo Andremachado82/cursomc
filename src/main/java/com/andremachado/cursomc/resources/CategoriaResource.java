652c27554d5321947707d1c4f3e29f42b08d52a9
André Machado <andrels.machado82@gmail.com>
2024-03-17 11:42:22 -0300

    Disponilizar Access-Control-Allow-Origin para o frontend

--- src/main/java/com/andremachado/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andremachado/cursomc/resources/CategoriaResource.java
@@ -10,15 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andremachado.cursomc.domain.Categoria;
@@ -54,7 +46,8 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(categoria);
 	}
-	
+
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<CategoriaDto>> findAll() {
 
